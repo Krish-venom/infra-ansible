e0dbf2c7f175e1b0ee5a9a47973191a783049ae1
Krishna <145960177+Krish-venom@users.noreply.github.com>
2026-02-01 17:45:18 +0000

    Refine variable descriptions and add validations
    Updated descriptions and added validations for variables in variables.tf.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,3 @@
-# variables.tf
-
 variable "aws_region" {
   description = "AWS region where resources will be created"
   type        = string
@@ -10,7 +8,7 @@ variable "apache_instance_count" {
   description = "Number of Apache web server instances"
   type        = number
   default     = 2
-  
+
   validation {
     condition     = var.apache_instance_count > 0 && var.apache_instance_count <= 10
     error_message = "Apache instance count must be between 1 and 10."
@@ -21,7 +19,7 @@ variable "nginx_instance_count" {
   description = "Number of Nginx web server instances"
   type        = number
   default     = 2
-  
+
   validation {
     condition     = var.nginx_instance_count > 0 && var.nginx_instance_count <= 10
     error_message = "Nginx instance count must be between 1 and 10."
@@ -35,19 +33,23 @@ variable "instance_type" {
 }
 
 variable "ami_id" {
-  description = "AMI ID for Ubuntu 22.04 LTS in the specified region"
+  description = "Your AMI ID (must exist in selected region)."
   type        = string
-  default     = "ami-019715e0d74f695be"  # Ubuntu 22.04 LTS in us-east-1
-  
+  default     = "ami-019715e0d74f695be"
+
+  validation {
+    condition     = length(var.ami_id) > 0 && can(regex("^ami-[0-9a-fA-F]{8,}$", var.ami_id))
+    error_message = "Provide a valid AMI ID (e.g., ami-xxxxxxxx) that exists in your chosen region."
+  }
 }
 
 variable "jenkins_ip" {
-  description = "Public IP address of Jenkins server (for SSH access)"
+  description = "Public IPv4 of Jenkins server (bare IP, no scheme/port), e.g., 13.201.85.232"
   type        = string
-  
+
   validation {
     condition     = can(regex("^\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}$", var.jenkins_ip))
-    error_message = "Jenkins IP must be a valid IPv4 address."
+    error_message = "Jenkins IP must be a valid IPv4 address like 13.201.85.232 (no http:// or port)."
   }
 }
 
@@ -68,3 +70,4 @@ variable "project_name" {
   type        = string
   default     = "devops-webapp"
 }
+``
